b92796115c2811a49ffd48c43fd528b87fb40c2f
Volker von Hoesslin <volker.hoesslin@swsn.de>
2020-04-03 16:52:31 +0200

    better handling for sub classes

--- src/AttributeHandler/StringAttributeHandler.php
+++ src/AttributeHandler/StringAttributeHandler.php
@@ -36,12 +36,21 @@ class StringAttributeHandler implements AttributeHandlerInterface
      */
     public function serializeValue(AttributeInterface $attribute, RequestPacket $requestPacket)
     {
-        $value = $attribute->getValue();
+        return $this->packValue($attribute->getValue());
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    protected function packValue(string $value)
+    {
         switch (true) {
             case (bool)preg_match(self::REGEX_HEX_VALUE, $value): // eg.: 0xFF00AACCBB55DD
                 return hex2bin(substr($value, 2)); //@todo: maybe faster: return hex2bin(base_convert($value, 16, 16));
-            case (bool)preg_match(self::REGEX_BIN_VALUE, $value): // eg.: 0b11010010000110101
-                return hex2bin(base_convert($value, 2, 16)); //@todo: is there any better option available?
+            // binary-string is just an feature, we can activate it maybe later...
+//            case (bool)preg_match(self::REGEX_BIN_VALUE, $value): // eg.: 0b11010010000110101
+//                return hex2bin(base_convert($value, 2, 16)); //@todo: is there any better option available?
             default:
                 return $value;
         }

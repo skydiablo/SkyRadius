77c9b2b84a98840aa208f5e020dbd0e90af46cb9
Volker <skydiablo@gmx.net>
2023-08-22 11:30:36 +0200

    add exception

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace SkyDiablo\SkyRadius\Exception;
+
+use SkyDiablo\SkyRadius\Exception\SkyRadiusException;
+
+class InvalidArgumentException extends SkyRadiusException
+{
+
+}
